a35c5f4e882b0f7110e6cfcf34412ce59242dfd1
Challagulla Dedipya <dedipyac@example.com>
2026-05-04 11:57:10 +0530

    Fix Vercel serverless function to include restaurants.db and proxy paths

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       return [
         {
           source: "/api/v1/:path*",
-          destination: "http://127.0.0.1:8000/v1/:path*",
+          destination: "http://127.0.0.1:8000/api/v1/:path*",
         },
       ];
     }
