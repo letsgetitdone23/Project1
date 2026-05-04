5d500db9645b481efa5e5f780e36705adf6c501b
Challagulla Dedipya <dedipyac@example.com>
2026-05-04 12:44:53 +0530

    Update next.config.js to proxy api requests to Vercel Serverless Function in production

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    if (process.env.NODE_ENV !== "production") {
-      return [
-        {
-          source: "/api/v1/:path*",
-          destination: "http://127.0.0.1:8000/api/v1/:path*",
-        },
-      ];
-    }
-    return [];
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
   },
 };
 
